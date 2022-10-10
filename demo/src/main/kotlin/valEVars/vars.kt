4fd37d6be96065346d16a1ffc29731e5e21b3eaf
joelma <joelmadev@gmail.com>
2022-10-10 17:31:00 -0300

    vars

---
+++ demo/src/main/kotlin/valEVars/vars.kt
@@ -0,0 +1,26 @@
+package valEVars
+
+fun main() {
+    var contaAlex = 12
+    var alex = ""
+    var modelCar: String?=null
+    var conta = 1000
+    var numeroAlex = 200.0
+    var jogo: Boolean = true
+
+    if (jogo == true && contaAlex != 12) {
+        println("Jogo: $contaAlex")
+    } else {
+        println(contaAlex)
+        println(alex)
+        println(modelCar)
+        println(conta)
+        println(numeroAlex)
+        println(jogo)
+
+
+    }
+
+
+
+}
